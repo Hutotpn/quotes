becce7cd385e1582e4582da4e2db4fa085cfb41b
Huto <huto25531112@gmail.com>
2025-11-23 14:31:04 +0000

    Init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import quotes from "./quotes.json";
+
+export default {
+	async fetch(request, env, ctx) {
+		if (request.method !== "GET") {
+			return new Response("Method Not Allowed", { status: 405 });
+		}
+
+		const randomIndex = Math.floor(Math.random() * quotes.length);
+		const randomQuote = quotes[randomIndex];
+
+		return new Response(JSON.stringify(randomQuote), {
+			headers: { "Content-Type": "application/json" },
+		});
+	},
+};
